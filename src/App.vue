278c046959335e132324e10e17b9acb3ff88ac0d
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-11-06 14:01:26 +0200

    create a project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+  <script>
+  import { ref } from 'vue';
+
+  export default {
+    setup() {
+      const start = ref(0)
+
+      return {
+        start
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+
+<style></style>
