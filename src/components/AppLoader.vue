2a1c4caffc2c065ce7592ba6a1c4118b81ce1ae7
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-11-07 00:30:59 +0200

    add loader component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="loader-overlay">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style>
+.loader-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  border: 8px solid rgba(255, 255, 255, 0.3);
+  border-top: 8px solid #ffffff;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
